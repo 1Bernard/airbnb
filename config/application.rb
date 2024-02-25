aeba9811cd44f5a2080f240bb7b747737255761e
Bernard <bernardansah5@gmail.com>
2024-02-25 00:14:34 +0000

    database configuration

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+require 'dotenv/load' if %w[development test].include? ENV['RAILS_ENV']
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
